d30e996913fba43921152610420d07307ef1bc35
soham1508 <116937878+soham1508@users.noreply.github.com>
2022-12-14 23:38:04 +0530

    Update delete_at_beg.c

--- delete_at_beg.c
+++ delete_at_beg.c
@@ -11,3 +11,14 @@ struct Node * deleteFirst(struct Node * head){
     free(ptr);
     return head;
 }
+int main(){
+	struct node*head;
+	head=(struct node*)malloc(sizeof(struct node));
+	third = (struct Node *)malloc(sizeof(struct Node));
+	 head->data = 1;
+    head->next = third;
+	 third->data = 41;
+         third->next = NULL;
+	head=deleteFirst(head);
+	return 0;
+}
